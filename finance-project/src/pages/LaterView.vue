956f557be6daf34ad3deef3e12e044396689a80c
surin <tjdtnfls1128@gmail.com>
2025-05-16 15:14:31 +0900

    [Refactor] store에 기능, getters 추가 #3

--- finance-project/src/pages/LaterView.vue
+++ finance-project/src/pages/LaterView.vue
@@ -4,11 +4,11 @@
     </div>
 
     <!-- 여기에 동영상 쭉 나열 -->
-    <div v-if="SavedVideo.length > 0">
+    <div v-if="store.savedVideo.length > 0">
         <!-- 저장된 비디오만 출력할 수 있도록 -->
-        <VideoDetail
+        <Video
             v-for="video in store.savedVideo"
-            :key="video.id"
+            :key="video.id.videoId"
             :video="video"
         />
     </div>
@@ -19,16 +19,15 @@
 
 <script setup>
     // 로컬 스토리지 사용
-    // import { useVideoStore } from "@/stores/video.js";
-    // const store = useVideoStore()
-
-    // 저장된 비디오 찾기 >> store로 들어갈 예정
-    // const savedVideo = store.videos.filter((video) => video.isSaved === true)
+    import { useAppStore } from "@/stores/app.js";
+    import Video from "@/components/Video.vue";
     
-    // 삭제하기 >> 나중에 VideoDetail로 넘어가야 함.
-    // const deleteVideo = function (selectedId) {
-    //     store.deleteVideo(selectedId)
-    // }
+    const store = useAppStore()
+    
+    // 삭제하기 >> 나중에 버튼에 넣어주기.
+    const deleteVideo = function (selectedId) {
+        store.deleteVideo(selectedId)
+    }
 </script>
 
 <style scoped>
