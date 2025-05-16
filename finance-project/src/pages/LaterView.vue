5e585088b126728f97ef4d1f2e12a8c9b06f04d3
surin <tjdtnfls1128@gmail.com>
2025-05-16 16:39:54 +0900

    [Refactor] 좋아요, 나중에 볼 영상 기능 수정 #3

--- finance-project/src/pages/LaterView.vue
+++ finance-project/src/pages/LaterView.vue
@@ -4,10 +4,10 @@
     </div>
 
     <!-- 여기에 동영상 쭉 나열 -->
-    <div v-if="store.savedVideo.length > 0">
+    <div v-if="store.savedVideos.length > 0">
         <!-- 저장된 비디오만 출력할 수 있도록 -->
         <Video
-            v-for="video in store.savedVideo"
+            v-for="video in store.savedVideos"
             :key="video.id.videoId"
             :video="video"
         />
@@ -24,10 +24,6 @@
     
     const store = useAppStore()
     
-    // 삭제하기 >> 나중에 버튼에 넣어주기.
-    const deleteVideo = function (selectedId) {
-        store.deleteVideo(selectedId)
-    }
 </script>
 
 <style scoped>
--- finance-project/src/stores/app.js
+++ finance-project/src/stores/app.js
@@ -25,6 +25,12 @@ export const useAppStore = defineStore("app", () => {
     try {
       const res = await axios.get(API_URL, { params });
       videos.value = res.data.items;
+
+      for (let i=0; i<videos.value.length; i++) {
+        videos.value[i].isSaved = false
+        videos.value[i].isLiked = false
+      }
+      
       console.log(videos.value);
     } catch (error) {
       console.error("검색 실패:", error);
@@ -32,9 +38,17 @@ export const useAppStore = defineStore("app", () => {
   };
 
   // 저장된 비디오 찾는 getters
-  const savedVideo = computed(() => {
+  const savedVideos = computed(() => {
     return videos.value.filter((video) => video.isSaved === true)
   })
+
+
+  // 비디오 저장
+  const saveVideo = function (selectedId) {
+    // isSaved 처리하기
+    const video = videos.value.find((video) => video.id.videoId === selectedId)
+    video.isSaved = true
+  }
 
   // 채널 삭제하기 >> 제거
   const deleteChannel = function (selectedId) {
@@ -49,12 +63,21 @@ export const useAppStore = defineStore("app", () => {
       videos.value = videos.value.filter((video) => video.id.videoId !== selectedId)
   }
 
+  // 비디오 좋아요
+  const likeVideo = function (selectedId) {
+    // isSaved 처리하기
+    const video = videos.value.find((video) => video.id.videoId === selectedId)
+    video.isLiked = true
+  }
+
   return {
     videos,
     channels,
+    savedVideos,
     searchKeyword,
-    savedVideo,
+    saveVideo,
     deleteVideo,
+    likeVideo,
     deleteChannel,
 
   };
